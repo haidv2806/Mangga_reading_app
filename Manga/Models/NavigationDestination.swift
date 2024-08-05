d4c503b6e3d54bd80a0754b28718f1f62286c71d
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-08-05 17:33:23 +0700

    thêm enum và funtion hỗ trợ việc điều hướng giao diện

---
+++ Manga/Models/NavigationDestination.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationDestination.swift
+//  Manga
+//
+//  Created by Đỗ Văn Hải on 5/8/24.
+//
+
+import SwiftUI
+
+struct NavigationDestination: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationDestination()
+}
