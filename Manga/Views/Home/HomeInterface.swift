da85ddb7322dd83c5a1644d5ed7bb21d3da0da6b
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-08-13 09:59:53 +0700

    Hoàn thiện việc show thông tin của mangga khi bấm vào mangga bất kỳ

--- Manga/Views/Home/HomeInterface.swift
+++ Manga/Views/Home/HomeInterface.swift
@@ -9,25 +9,25 @@ import SwiftUI
 
 struct HomeInterface: View {
     @StateObject var viewModel = MangaViewModel()
-    
     @State private var activeNavigation: NavigationDestination?
-    
     @Binding var selectedCategory: String
     
+    @State private var showDetailView: Bool = false
+    @State private var selectedManga: MangaCategory? = nil
+    @Namespace private var animation
+    
     var body: some View {
+        if #available(iOS 15.0, *) {
         NavigationView {
             VStack{
                 HomeTop()
                 
-                if #available(iOS 15.0, *) {
-                    ListManga(mangas: viewModel.mangaCategories)
+
+                    ListManga(showDetailView: $showDetailView, selectedManga: $selectedManga, mangas: viewModel.mangaCategories)
                         .task {
                             await viewModel.fetchCategoryMangas(category: selectedCategory)
                         }
-                } else {
-                    // Fallback for earlier versions
-                    Text("task is only available on iOS 15 or newer.")
-                }
+
                 
                 TaskBar(User_Image: "UserImg", navigationDestination: $activeNavigation)
                 if let activeNavigation = activeNavigation {
@@ -36,10 +36,27 @@ struct HomeInterface: View {
                 
             }
             .padding(sides: [.left, .right], value: 17)
+            
+            .overlay {
+                if let selectedManga, showDetailView {
+                    DetailView(show: $showDetailView, animation: animation, manga: selectedManga, link: selectedManga.link)
+                        .transition(.asymmetric(insertion: .identity, removal: .offset(y: 5)))
+                } else {
+                    EmptyView() // Return an EmptyView if the condition is not met
+                }
+            }
+            
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
+            
+
+            
 //        .ignoresSafeArea()
+        } else {
+            // Fallback for earlier versions
+            Text("task is only available on iOS 15 or newer.")
+        }
     }
 }
 
--- Manga/Views/Home/MangaList/ListManga.swift
+++ Manga/Views/Home/MangaList/ListManga.swift
@@ -8,18 +8,45 @@
 import SwiftUI
 
 struct ListManga: View {
-    @State private var showDetailView: Bool = false
-    @State private var selectedManga: MangaCategory?
+//    @State private var showDetailView: Bool = false
+//    @State private var selectedManga: MangaCategory?
+    @Binding var showDetailView: Bool
+    @Binding var selectedManga: MangaCategory?
+    @State private var animateCurrentManga: Bool = false
+    @Namespace private var animation
     
     var mangas: [MangaCategory]
     
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVGrid(columns: [GridItem(), GridItem()], content: {
-                ForEach(mangas) { manga in
-                    SpecificManga(Manga_title: manga.title, Manga_authorHome: manga.authorHome, Manga_poster: manga.poster.absoluteString)
+        if #available(iOS 15.0, *) {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: [GridItem(), GridItem()], content: {
+                    ForEach(mangas) { manga in
+                        SpecificManga(Manga_title: manga.title, Manga_authorHome: manga.authorHome, Manga_poster: manga.poster.absoluteString)
+                            .onTapGesture {
+                                withAnimation {
+                                    selectedManga = manga
+                                    showDetailView = true
+                                }
+                            }
+                    }
+                })
+            }
+//            .overlay {
+//                if let selectedManga, showDetailView {
+//                    DetailView(show: $showDetailView, animation: animation, manga: selectedManga, link: selectedManga.link)
+//                        .transition(.asymmetric(insertion: .identity, removal: .offset(y: 5)))
+//                }
+//            }
+            .onChange(of: showDetailView) { newValue in
+                if !newValue {
+                    withAnimation(.easeInOut(duration: 0.15).delay(0.3)) {
+                        animateCurrentManga = false
+                    }
                 }
-            })
+            }
+        } else {
+            
         }
     }
 }
@@ -28,19 +55,20 @@ struct ListManga: View {
 
 
 
+
 struct ListManga_preview: PreviewProvider {
     static var previews: some View {
         @StateObject var viewModel = MangaViewModel()
+        @State var showDetailView = false
+        @State var selectedManga: MangaCategory? = nil
         
         if #available(iOS 15.0, *) {
-            ListManga(mangas: viewModel.mangaCategories)
+            ListManga(showDetailView: $showDetailView, selectedManga: $selectedManga, mangas: viewModel.mangaCategories)
                 .task {
                     await viewModel.fetchCategoryMangas(category: "")
                 }
         } else {
-            // Fallback for earlier versions
             Text("task is only available on iOS 15 or newer.")
         }
-        
     }
 }
